504e4a529da47e1e718270ce4a10b972019260cc
kleidis <167202775+kleidis@users.noreply.github.com>
2024-08-08 18:59:37 +0200

    bash: fix clean command

--- hosts/default/modules/config/bash.nix
+++ hosts/default/modules/config/bash.nix
@@ -15,7 +15,7 @@ in
       update = "cd ~/nixos && nix flake update";
       clean = ''
         nix-collect-garbage --delete-old;
-        nix-env -p /nix/var/nix/profiles/system --delete-generations old;
+        sudo nix-collect-garbage --delete-older-than 30d;
         sudo nix-collect-garbage -d;
         sudo /run/current-system/bin/switch-to-configuration boot;
       '';
